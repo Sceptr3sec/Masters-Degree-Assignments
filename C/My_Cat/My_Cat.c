b1974ac5d1ebd360e072dc188b4d7e3535aefc74
Sceptr3sec <prot0securesceptr3@gmail.com>
2025-08-08 12:42:39 -0500

    Update My_Cat.c

--- C/My_Cat/My_Cat.c
+++ C/My_Cat/My_Cat.c
@@ -3,18 +3,18 @@
 
 int main(int argc, char *argv[]) {
     FILE *file;
-    char buffer[100]; //Buffer Overflow vulnerability :)
+    char buffer[100]; //Vulnerability, yes I know :)
     size_t bytes_read;
 
     for (int i = 1; i < argc; i++) {
-        file = fopen(argv[i], "r");
+        file = fopen(argv[i], "r"); //yes, yes I know... vulneability
         if (file == NULL) {
             printf("Unable to open file: %s\n", argv[i]);
             continue;
         }
 
         while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-            fwrite(buffer, 1, bytes_read, stdout);
+            fwrite(buffer, 1, bytes_read, stdout); //Listen.. I'm just trying to pass ROFL
         }
 
         fclose(file);
